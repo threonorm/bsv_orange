5714e7ed90cc7436cb694e4f8c59ec13e52384e4
Your Name <you@example.com>
2023-05-03 00:43:28 -0400

    Add BlueAXI

--- TestBuildUsb.sh
+++ TestBuildUsb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 USBCORE=/home/bthom/git/bsv_orange/tinyfpga_bx_usbserial/
 WISHBONE=/home/bthom/git/Wishbone_BSV/src
+BLUEAXI=/home/bthom/git/BlueAXI/src
 rm -rf build/
 mkdir -p build
 
@@ -10,6 +11,6 @@ cp Makefile build/.
 cp usborange.pcf build/.
 cp litedram.v build/.
 
-bsc -p $WISHBONE:+ --aggressive-conditions -bdir build -vdir build -verilog -u OrangeCrabTop.bsv
+bsc -p $WISHBONE:$BLUEAXI+ --aggressive-conditions -bdir build -vdir build -verilog -u OrangeCrabTop.bsv
 sed -i '0,/\.pin_usb_p/{s/\.pin_usb_p(usb_core$pin_usb_p)/usb_core$pin_usb_p/}' build/top.v
 sed -i '0,/\.pin_usb_n/{s/\.pin_usb_n(usb_core$pin_usb_n)/usb_core$pin_usb_n/}' build/top.v
