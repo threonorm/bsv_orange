3fa41c32123a7753afb70037ce17430f5720de85
Your Name <you@example.com>
2023-05-02 22:48:48 -0400

    Add wishbone

--- TestBuildUsb.sh
+++ TestBuildUsb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 USBCORE=/home/bthom/git/bsv_orange/tinyfpga_bx_usbserial/
+WISHBONE=/home/bthom/git/Wishbone_BSV/src
 rm -rf build/
 mkdir -p build
 
@@ -8,6 +9,6 @@ cp $USBCORE/*.v build/.
 cp Makefile build/.
 cp usborange.pcf build/.
 
-bsc --aggressive-conditions -bdir build -vdir build -verilog -u OrangeCrabTop.bsv
+bsc -p $WISHBONE:+ --aggressive-conditions -bdir build -vdir build -verilog -u OrangeCrabTop.bsv
 sed -i '0,/\.pin_usb_p/{s/\.pin_usb_p(usb_core$pin_usb_p)/usb_core$pin_usb_p/}' build/top.v
 sed -i '0,/\.pin_usb_n/{s/\.pin_usb_n(usb_core$pin_usb_n)/usb_core$pin_usb_n/}' build/top.v
